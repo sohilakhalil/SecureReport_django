f7f23569414404122a4226de050822fcde7fd6fd
sohilakhalil <sohilakhalil283@gmail.com>
2025-09-23 21:38:18 +0300

    Initial commit - SecureReport user module

--- crime_report_system/settings.py
+++ crime_report_system/settings.py
@@ -45,15 +45,17 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",   
-    "django.middleware.common.CommonMiddleware",
+    "reports.middleware.CorsFixMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 CORS_ALLOWED_ORIGINS = [
     "https://securereport.netlify.app",
